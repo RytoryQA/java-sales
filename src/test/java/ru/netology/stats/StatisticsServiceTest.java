51eca07eb02b2084dc32c48ff2705233e2fc471f
RytoryQA <vikaryzhkina89@gmail.com>
2023-07-13 02:23:57 +0400

    Revision-2

--- src/test/java/ru/netology/stats/StatisticsServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticsServiceTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
-    int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+  
 
     @Test
 
     public void testGetAmountAllSales() {
         StatisticsService service = new StatisticsService();
-
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 180;
         int actual = service.getAmountAllSales(sales);
         Assertions.assertEquals(expected, actual);
@@ -18,7 +18,7 @@ public class StatisticsServiceTest {
 
     public void testGetAverageMonthlySales() {
         StatisticsService service = new StatisticsService();
-
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 15;
         int actual = service.getAverageMonthlySales(sales);
         Assertions.assertEquals(expected, actual);
@@ -26,7 +26,7 @@ public class StatisticsServiceTest {
 
     public void testGetMonthMaxSales() {
         StatisticsService service = new StatisticsService();
-
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 8;
         int actual = service.getMonthMaxSales(sales);
         Assertions.assertEquals(expected, actual);
@@ -34,7 +34,7 @@ public class StatisticsServiceTest {
 
     public void testGetMonthMinSales() {
         StatisticsService service = new StatisticsService();
-
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 9;
         int actual = service.getMonthMinSales(sales);
         Assertions.assertEquals(expected, actual);
@@ -42,7 +42,7 @@ public class StatisticsServiceTest {
 
     public void testGetMonthsBellowAverage() {
         StatisticsService service = new StatisticsService();
-
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 5;
         int actual = service.getMonthsBellowAverage(sales);
         Assertions.assertEquals(expected, actual);
@@ -50,7 +50,7 @@ public class StatisticsServiceTest {
 
     public void testGetMonthsAboveAverage() {
         StatisticsService service = new StatisticsService();
-
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 5;
         int actual = service.getMonthsAboveAverage(sales);
         Assertions.assertEquals(expected, actual);
